7159ad0169237641ddcfa863dd8120c758823eda
Noelby13 <hnoelreyes25@gmail.com>
2024-02-22 13:31:56 -0600

    add(FrmPrincipal): se reduce tamaño de la barra de menu, se agrega un pictureBox para la foto de perfil del usuario

--- formularios/FrmUsuario.Designer.cs
+++ formularios/FrmUsuario.Designer.cs
@@ -33,19 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(106, 132);
+            this.button1.Location = new System.Drawing.Point(141, 162);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 0;
             this.button1.Text = "soy usuario";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // FrmUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FrmUsuario";
             this.Text = "FrmUsuario";
             this.ResumeLayout(false);
